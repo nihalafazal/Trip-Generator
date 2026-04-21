dc3de763563551471d84fe286311d438bff5dc9a
nihalafazal <nihalafazal25@gmail.com>
2026-04-21 19:49:23 +0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 
 def load_prompt_template():
